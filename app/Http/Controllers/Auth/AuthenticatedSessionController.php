3baea79e2ee653e7acba6df2335c120303e5e81c
orchid06 <orchid.b27@gmail.com>
2025-01-03 00:26:25 +0600

    added multi auth login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,20 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $url = '';
+
+
+        if($request->user()->role === 'admin'){
+            $url = 'admin/dashboard';
+        }elseif($request->user()->role === 'agent'){
+            $url =  'agent/dashboard';
+        }elseif($request->user()->role === 'user'){
+            $url =  '/dashboard';
+        };
+
+        // return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended($url);
+
     }
 
     /**
