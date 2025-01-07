dde1a215443057896247b6fb419b267bf1af00ca
orchid06 <orchid.b27@gmail.com>
2025-01-08 01:55:05 +0600

    added admin password update functionality

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -67,4 +68,43 @@ class AdminController extends Controller
 
         return redirect()->back()->with($notification);
     } //End method
+
+    public function AdminChangePassword()
+    {
+        $id             = Auth::user()->id;
+        $profileData    = User::find($id);
+
+        return view('admin.admin_change_password', compact('profileData'));
+    }
+
+    public function AdminUpdatePassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        //Match the old password
+        if(!Hash::check($request->old_password , Auth::user()->password)){
+
+            $notification = [
+                'message'       => 'Old password does not match',
+                'alert-type'    => 'error'
+            ];
+
+            return back()->with($notification);
+        }
+
+        //Update the new password
+        User::whereId(Auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        $notification = [
+            'message'       => 'password change successfully',
+            'alert-type'    => 'success'
+        ];
+
+        return redirect()->back()->with($notification);
+    }
 }
